9609bb30a905121dec93dd48692ba57cf44c8e16
Simon Lang <me@simonlang.org>
2017-01-31 19:52:10 +1000

    First draft parser

---
+++ app.js
@@ -0,0 +1,62 @@
+// brew install poppler
+// pdftotext input.pdf
+// node app.js > output.csv
+
+const _ = require('lodash')
+const fs = require('fs')
+
+let output = {}
+
+let files = fs.readdirSync('./txt')
+files.forEach((file) => {
+    let years = {}
+    let currentYear = null
+    let text = fs.readFileSync('txt/' + file, 'utf8')
+    let lines = text.split('\n')
+
+    lines.forEach((line) => {
+        let year = parseFloat(line)
+        if (year > 1900 && year < 2050) {
+            currentYear = year
+            years[year] = []
+        }
+        if (years[currentYear]) {
+            let arr = line.split(',').map(x => x.trim())
+            if (arr.length < 4) {
+                return true
+            }
+            years[currentYear].push({
+                title: arr[0],
+                gallery: arr[1],
+                location: arr[2],
+                country: arr[3]
+            })
+        }
+    })
+
+
+    let artist = file.replace('.txt', '')
+    output[artist] = years
+})
+
+const encode = (s) => {
+    s = s.replace(/"/g, '\"')
+    return `"${s}"`
+}
+
+let csv = '"artist","year","title","gallery","location","country"\n'
+_.each(output, (record, artist) => {
+    _.each(record, (item, year) => {
+        _.each(item, (show) => {
+            csv += encode(artist) + ','
+            csv += encode(year) + ','
+            csv += encode(show.title) + ','
+            csv += encode(show.gallery) + ','
+            csv += encode(show.location) + ','
+            csv += encode(show.country)
+            csv += '\n'
+        })
+    })
+})
+
+console.log(csv)
