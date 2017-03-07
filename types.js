7fb7f3f0a5af3a7b3e702b435619a5344dd0ebfc
Simon Lang <me@simonlang.org>
2017-03-07 19:57:40 +1000

    Countries and Show Types lists

---
+++ types.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'Award': [
+    'awards',
+    'accolades',
+    'recognitions',
+  ],
+  'Solo Show': [
+    'solo shows',
+    'selected solo shows',
+    'solo exhibitions',
+    'selected solo exhibitions',
+  ],
+  'Group Show': [
+    'group shows',
+    'selected group shows',
+    'group exhibitions',
+    'selected group exhibitions',
+  ],
+  'Public Collection': [
+    'public collections',
+    'selected public collections',
+  ],
+}
