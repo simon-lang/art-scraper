c77be8931df73c801da9a399b8b1908d488795c2
Simon Lang <me@simonlang.org>
2017-03-09 19:16:40 +1000

    Basic country/location disambiguation
    Also:
    - make csv writer a module
    - linter autoformat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// brew install poppler
-// pdftotext input.pdf
-// node app.js > output.csv
-
 const _ = require('lodash')
 const fs = require('fs')
 const argv = require('yargs').argv
@@ -9,79 +5,70 @@ const argv = require('yargs').argv
 const countries = require('./countries')
 const countryNames = _.values(countries)
 const types = require('./types')
+const csvWriter = require('./csv')
 
 let output = {}
 
 let files = fs.readdirSync('./txt')
 files.forEach((file) => {
-    let years = {}
-    let currentType = null
-    let currentYear = null
-    let text = fs.readFileSync('txt/' + file, 'utf8')
-    let lines = text.split('\n')
+  let years = {}
+  let currentType = null
+  let currentYear = null
+  let text = fs.readFileSync('txt/' + file, 'utf8')
+  let lines = text.split('\n')
 
-    lines.forEach((line) => {
-        line = line.trim()
-        _.each(types, (aliases, type) => {
-          if (line.toLowerCase() === type || _.includes(aliases, line.toLowerCase())) {
-            currentType = type
-          }
-        })
-        let year = parseFloat(line)
-        if (year > 1900 && year < 2050) {
-            currentYear = year
-            if (years[year] === undefined) {
-              years[year] = []
-            }
-        }
-        if (years[currentYear]) {
-            let arr = line.split(',').map(x => x.trim())
-            if (arr.length < 3) {
-                return true
-            }
-            // TODO: if (_.includes(countryNames, arr[3])), etc.
-            let item = {
-                title: arr[0],
-                gallery: arr[1],
-                location: arr[2],
-                country: arr[3]
-            }
-            if (currentType) {
-              item.type = currentType
-            }
-            years[currentYear].push(item)
-        }
+  lines.forEach((line) => {
+    line = line.trim()
+    _.each(types, (aliases, type) => {
+      if (line.toLowerCase() === type || _.includes(aliases, line.toLowerCase())) {
+        currentType = type
+      }
     })
+    let year = parseFloat(line)
+    if (year > 1900 && year < 2050) {
+      currentYear = year
+      if (years[year] === undefined) {
+        years[year] = []
+      }
+    }
+    if (years[currentYear]) {
+      let arr = line.split(',').map(x => x.trim())
+      if (arr.length < 3) {
+        return true
+      }
 
-    let artist = file.replace('.txt', '')
-    output[artist] = years
+      const [title, gallery] = arr
+      let location
+      let country
+      if (_.includes(countryNames, arr[3])) {
+        location = arr[2]
+        country = arr[3]
+      } else if (_.includes(countryNames, arr[2])) {
+        country = arr[2]
+      }
+      let item = {
+        title,
+        gallery,
+        location,
+        country,
+      }
+      if (currentType) {
+        item.type = currentType
+      }
+      years[currentYear].push(item)
+    }
+  })
+
+  let artist = file.replace('.txt', '')
+  output[artist] = years
 })
 
-const json = JSON.stringify(output)
-
-const encode = (s) => {
-    s = s.replace(/"/g, '\"')
-    return `"${s}"`
-}
-
-if (argv.json) {
+if (argv.silent) {
+  // noop
+} else if (argv.json) {
+  const json = JSON.stringify(output)
   console.log(json)
-}
-else {
-  let csv = '"artist","year","type","title","gallery","location","country"\n'
-  _.each(output, (record, artist) => {
-      _.each(record, (item, year) => {
-          _.each(item, (show) => {
-              csv += encode(artist) + ','
-              csv += encode(year) + ','
-              csv += encode(show.type || '') + ','
-              csv += encode(show.title) + ','
-              csv += encode(show.gallery) + ','
-              csv += encode(show.location) + ','
-              csv += encode(show.country || '')
-              csv += '\n'
-          })
-      })
-  })
+} else {
+  const csv = csvWriter(output)
   console.log(csv)
 }
---
+++ src/csv.js
@@ -0,0 +1,25 @@
+const _ = require('lodash')
+
+const encode = (s = '') => {
+  s = s.replace(/"/g, '\"')
+  return `"${s}"`
+}
+
+module.exports = (data) => {
+  let csv = '"artist","year","type","title","gallery","location","country"\n'
+  _.each(data, (record, artist) => {
+    _.each(record, (item, year) => {
+      _.each(item, (show) => {
+        csv += encode(artist) + ','
+        csv += encode(year) + ','
+        csv += encode(show.type || '') + ','
+        csv += encode(show.title) + ','
+        csv += encode(show.gallery) + ','
+        csv += encode(show.location) + ','
+        csv += encode(show.country || '')
+        csv += '\n'
+      })
+    })
+  })
+  return csv
+}
